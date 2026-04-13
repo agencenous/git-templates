972ac9ff2ce32a4622d2deff795535dadedcb702
Bastien Ho <bastien@avecnous.eu>
2026-04-13 16:09:23 +0200

    Add composer plugin

---
+++ src/ComposerPlugin.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AgenceNous\GitTemplates;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+class ComposerPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => 'installTemplates',
+            ScriptEvents::POST_UPDATE_CMD => 'installTemplates',
+        ];
+    }
+
+    public function installTemplates(Event $event): void
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        $binScript = dirname($vendorDir) . '/vendor/agencenous/git-templates/bin/git-templates';
+
+        if (!file_exists($binScript)) {
+            return;
+        }
+
+        $event->getIO()->write('<info>agencenous/git-templates:</info> Installing GitLab issue templates...');
+
+        $process = new \Symfony\Component\Process\Process(
+            ['php', $binScript, '--project-dir=' . dirname($vendorDir)],
+            dirname($vendorDir),
+        );
+        $process->run(function (string $type, string $buffer) use ($event): void {
+            $event->getIO()->write($buffer, false);
+        });
+    }
+}
